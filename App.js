d02cca403ba3fbe98c8114cc901174d9781c2b1d
koleks92 <koleks92@gmail.com>
2023-10-05 20:28:26 +0200

    Button + notificationHandler

--- App.js
+++ App.js
@@ -1,10 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import * as Notifications from 'expo-notifications';
 
 export default function App() {
+  function scheduleNotificationHandler() {
+    Notifications.scheduleNotificationAsync({
+      content: {
+        title: "Hello",
+        body: 'This is body of HelloNotification',
+        data: { userName: 'Jan' }
+      },
+      trigger: {
+        seconds: 10
+      }
+    });
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Button title="Schedule Notification" onPress={scheduleNotificationHandler}/>
       <StatusBar style="auto" />
     </View>
   );
